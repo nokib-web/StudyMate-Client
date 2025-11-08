c2d07da6684d7db68ac7c9020d1ae58bebf2b740
nokib-web <nokibnokib1@gmail.com>
2025-11-08 22:13:03 +0600

    added env for firebase authentication api key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAaWshR-CwKKdJpmUzSek30Pjn8F-BZpCI",
-  authDomain: "studymate-b37fa.firebaseapp.com",
-  projectId: "studymate-b37fa",
-  storageBucket: "studymate-b37fa.firebasestorage.app",
-  messagingSenderId: "31376805033",
-  appId: "1:31376805033:web:8c9aac5ec8e140c51eedb8"
+
+  apiKey: import.meta.env.VITE_apiKey ,
+  authDomain: import.meta.env.VITE_authDomain ,
+  projectId: import.meta.env.VITE_projectId ,
+  storageBucket: import.meta.env.VITE_storageBucket ,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
