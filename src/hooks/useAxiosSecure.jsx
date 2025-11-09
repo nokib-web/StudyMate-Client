fd7a14fe3f9114bd7d1903cec30ca253c476e59a
nokib-web <nokibnokib1@gmail.com>
2025-11-09 17:05:39 +0600

    Unauthorized functionality implement

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000',
@@ -9,21 +10,49 @@ const instance = axios.create({
 
 
 const useAxiosSecure = () => {
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
 
 
     useEffect(() => {
+        // Add a request interceptor
         const requestInterceptor = instance.interceptors.request.use((config) => {
-            config.headers.authorization = `Bearer ${user.accessToken}`;
-            console.log("User access token:", user.accessToken);
+            const token = user.accessToken;
+            if (token) {
+                config.headers.authorization = `Bearer ${token}`;
+            }
+            console.log("User access token:", token);
             return config;
         });
 
+        // Response interceptor
+        const responseInterceptor = instance.interceptors.response.use(res => {
+            return res;
+        },
+            err => {
+                const status = err.status || err.response?.status;
+                if (status === 401 || status === 403) {
+                    // Handle unauthorized or forbidden responses
+                    console.log("Unauthorized access - logging out user.");
+                    // You can add logout logic here
+                    logout().then(() => {
+                        navigate("/login");
+                    })
+                }
+            }
+
+        )
+
+
+
+
+        // Eject the interceptor when the component unmounts or user changes
         return () => {
             instance.interceptors.request.eject(requestInterceptor);
+            instance.interceptors.response.eject(responseInterceptor);
         };
-        
-    }, [user]);
+
+    }, [user, logout, navigate]);
 
     return instance;
 }
