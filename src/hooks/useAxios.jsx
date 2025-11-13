d39aacb70a86a6702b4ba68890e75cf1e313d72c
nokib-web <nokibnokib1@gmail.com>
2025-11-13 17:27:52 +0600

    updates

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000', 
+    baseURL: 'https://study-mate-server-nazmul-hasan-nokibs-projects.vercel.app', 
+    headers: {
+        'Content-Type': 'application/json',
+    },  
 
 });
 
